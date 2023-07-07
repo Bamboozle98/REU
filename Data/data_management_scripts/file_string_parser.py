7d1a78b3b904871efef8e7ba97e4f63d5b9dbf36
Bamboozle98 <89475103+Bamboozle98@users.noreply.github.com>
2023-07-07 11:18:10 -0400

    Update file_string_parser.py

--- Data/data_management_scripts/file_string_parser.py
+++ Data/data_management_scripts/file_string_parser.py
@@ -5,7 +5,8 @@ import os
 # This code was written as the framework for parsing through my database file directory to automatically scrape images.
 # It iterates through the indexes 19 to end because I implemented this code after doing the first 20 directories one by
 # one.
-# This code was incorporated into the Google-Image_Scraper main.py file as well as the duplicate_image_obliterater.py
+# This code was incorporated into the Google-Image_Scraper main.py, the duplicate_image_obliterater.py, the directory_creation.py,
+# and the file_scrubber.py
 file_list = 'E:/My Data v.4/theropod/'
 
 for fname in os.listdir(file_list)[19:]:
